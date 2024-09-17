93fbe3ad166de79f837564188fc2257bc64d234d
Guillaume Belissent <guillaumebelissent@tsf-460-wpa-5-179.epfl.ch>
2024-09-17 12:13:53 +0200

    visualisations

--- implemetations.py
+++ implemetations.py
@@ -9,24 +9,33 @@ def MSE(y, pred):
 def MSE_gradient(y, tx, w):
     return -1/tx.shape[0]*np.inner(tx.T, y - np.inner(tx, w))
 
-def least_squares(y, tx):
-    tx = np.c_[np.ones((tx.shape[0], 1)), tx]
+def least_squares(y, tx, offset = False):
+    
+    # Adding offset
+    if offset:  
+        tx = np.c_[np.ones((tx.shape[0], 1)), tx]
+    
+    
     B = np.inner(np.dot(np.linalg.inv(np.dot(tx.T, tx)), tx.T), y)
     return B, MSE(y, np.inner(B, tx))
 
-def ridge_regression(y, tx, lambda_):
-    tx = np.c_[np.ones((tx.shape[0], 1)), tx]
+def ridge_regression(y, tx, lambda_, offset = False):
+    if offset:
+        tx = np.c_[np.ones((tx.shape[0], 1)), tx]
     print(tx.shape)
     B = np.inner(np.dot(np.linalg.inv(np.dot(tx.T, tx) + lambda_/(2*tx.shape[0]) * np.eye(tx.shape[1])), tx.T), y)
     return B, MSE(y, np.inner(B, tx))
 
 
 # Linear regression, MSE Loss, Gradient descent
-def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
+def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma, offset = False):
+    
+    # Adding offset
+    if offset:  
+        tx = np.c_[np.ones((tx.shape[0], 1)), tx]
     B = initial_w
     hist = []
-    # adding offset
-    tx = np.c_[np.ones((tx.shape[0], 1)), tx]
+
     
     for _ in range(max_iters):
         B -= gamma * MSE_gradient(y,tx,B)
@@ -34,11 +43,14 @@ def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
 
     return B, MSE(y, np.inner(B, tx)), hist
 
-def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma, batch = 2):
+def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma, batch = 2, offset = False):
+    
+    # Adding offset
+    if offset:  
+        tx = np.c_[np.ones((tx.shape[0], 1)), tx]
     B = initial_w
     hist = []
-    # adding offset
-    tx = np.c_[np.ones((tx.shape[0], 1)), tx]
+
     
     for _ in range(max_iters):
         idx = np.random.randint(tx.shape[0], size=batch)
